104a639094bd85336f9fbe14dcaa3bd86f244de4
DrSumo <n25nk1@outlook.com>
2022-03-01 10:05:05 -0600

    finished

---
+++ other.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+	//var greeting;
+	//var element = document.getElementById("intro");
+
+	var button = document.querySelector("button");
+	button.addEventListener("click", addText);
+
+	//element.insertAdjacentHTML("afterbegin", "h1", + greeting + "</h1>");
+
+	function addText() {
+		var textField = document.getElementById("textfield");
+		var msg = "<h1> This text was added!</h1>";
+		textField.innerHTML = msg;
+	}
+}
